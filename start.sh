717ebf2edab6d4439f15408792e21b8fe7270852
mvousden <mark.vousden@soton.ac.uk>
2016-11-02 23:52:56 +0000

    Add some port information to avoid clashes. There is probably still a problem in the protocols entry of the conf, since only one slave port is used...

--- start.sh
+++ start.sh
@@ -5,6 +5,10 @@ BUILDBOT_EXEC_PREFIX="$HOME/miniconda2/bin"
 $BUILDBOT_EXEC_PREFIX/buildbot-worker create-worker worker_1 localhost worker_1 pass_1
 $BUILDBOT_EXEC_PREFIX/buildbot-worker create-worker worker_2 localhost worker_2 pass_2
 
+# Change the port of the workers.
+sed "s|port = 9989|port = 9990|" -i worker_1/buildbot.tac
+sed "s|port = 9989|port = 9991|" -i worker_2/buildbot.tac
+
 # Start the master and the workers.
 $BUILDBOT_EXEC_PREFIX/buildbot start master/
 $BUILDBOT_EXEC_PREFIX/buildbot-worker start worker_1/
