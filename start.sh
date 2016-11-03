02ebc294372239188eef1262844adbd12ad753d0
mvousden <mark.vousden@soton.ac.uk>
2016-11-03 00:02:54 +0000

    Change how we write worker ports.

--- start.sh
+++ start.sh
@@ -2,12 +2,8 @@
 BUILDBOT_EXEC_PREFIX="$HOME/miniconda2/bin"
 
 # Create workers.
-$BUILDBOT_EXEC_PREFIX/buildbot-worker create-worker worker_1 localhost worker_1 pass_1
-$BUILDBOT_EXEC_PREFIX/buildbot-worker create-worker worker_2 localhost worker_2 pass_2
-
-# Change the port of the workers.
-sed "s|port = 9989|port = 9990|" -i worker_1/buildbot.tac
-sed "s|port = 9989|port = 9991|" -i worker_2/buildbot.tac
+$BUILDBOT_EXEC_PREFIX/buildbot-worker create-worker worker_1 localhost:9990 worker_1 pass_1
+$BUILDBOT_EXEC_PREFIX/buildbot-worker create-worker worker_2 localhost:9991 worker_2 pass_2
 
 # Start the master and the workers.
 $BUILDBOT_EXEC_PREFIX/buildbot start master/
